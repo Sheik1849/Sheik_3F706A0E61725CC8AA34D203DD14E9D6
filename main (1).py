cfcaef75f6f0485cde5e181673328de3d2a4c225
Sheik1849 <144330587+Sheik1849@users.noreply.github.com>
2023-09-08 14:59:24 +0530

    Add files via upload

--- main (1).py
+++ main (1).py
@@ -1,14 +1,10 @@
-# Default function to implement conditions to check leap year  
-def CheckLeap(Year):  
-  # Checking if the given year is leap year  
-  if((Year % 400 == 0) or  
-     (Year % 100 != 0) and  
-     (Year % 4 == 0)):   
-    print("Given Year is a leap Year");  
-  # Else it is not a leap year  
-  else:  
-    print ("Given Year is not a leap Year")  
-# Taking an input year from user  
-Year = int(input("Enter the number: "))  
-# Printing result  
-CheckLeap(Year)
+# Python code to demonstrate naive method
+# to compute factorial
+n = 23
+fact = 1
+ 
+for i in range(1, n+1):
+    fact = fact * i
+ 
+print("The factorial of 23 is : ", end="")
+print(fact)
--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-num = int(input("Enter a number: "))
-factorial = 1
-if num < 0:
-  print(" Factorial does not exist for negative numbers")
-elif num == 0:
-  print("The factorial of 0 is 1")
+year = 2000
+
+if (year%400 == 0) or (year%4==0 and year%100!=0):
+    print("Leap Year")
 else:
-  for i in range(1, num + 1):
-    factorial = factorial * i
-  print("The factorial of", num, "is", factorial)
+    print("Not a Leap Year")
